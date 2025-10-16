6ed0920e102f6e537d328b7b50518273ebd757cd
amher <amhercosainetimpahan@gmail.com>
2025-10-17 06:51:11 +0800

    added fixes for store and scanning

--- lib/pages/unified_store_dashboard.dart
+++ lib/pages/unified_store_dashboard.dart
@@ -227,4 +227,3 @@ class _UnifiedStoreDashboardState extends State<UnifiedStoreDashboard> {
     );
   }
 }
-
--- lib/seller_pages/store_form_page.dart
+++ lib/seller_pages/store_form_page.dart
@@ -386,4 +386,3 @@ class _StoreFormPageState extends State<StoreFormPage> {
     );
   }
 }
-
